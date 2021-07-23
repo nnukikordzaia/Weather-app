067550400b1eb8d6550d5e2e8cbf01873241c792
nnukikordzaia <anuki.kordzaia@azry.com>
2021-07-24 00:19:35 +0400

    weather app withouth useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const api={
+  key: "5141e7b91048915162033fd01b3f4f0f",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[city, setCity] = useState({})
+  const[weather, setWeather] = useState({})
+
+  const Search = evt => {
+      if(evt.key === "Enter"){
+      fetch(${api.base}weather?q=${city}&appid=${api.key})
+      .then(response => response.json())
+      .then(data =>{
+        setWeather(data);
+        setCity("");
+        console.log(data);
+    }).catch(alert("City not found"));
+  }
+}
+ 
+return (
+  <div className="App">
+    <div className="search-box">
+      <input 
+        type="text"
+        className="search-bar"
+        placeholder="Search city..."
+        onChange={ e => setCity(e.target.value) }
+        onKeyPress={Search }
+        value= {city}/>
     </div>
-  );
+    {(typeof weather.main != "undefined") ? (
+      <div className="weather-box">
+      <div className='location'>{weather.name}</div>
+      <div className='temp'>{weather.main.temp}*C</div>
+      </div>
+    ) : ('')}
+    
+  </div>
+);
 }
 
 export default App;
